7000c12e5ff60b3c303edab869d112a1e8a08166
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-11-28 17:15:41 -0200

    down case

---
+++ down_case_with_space.s
@@ -0,0 +1,37 @@
+global _start
+
+section .data
+snippet db 'KANGOROO ', 'CACHORRA', 0DH, 0AH
+size_snippet equ $-snippet
+
+section .text
+_start:
+
+
+mov eax, 4
+mov ebx, 1
+mov ecx, snippet
+mov edx, size_snippet
+int 80h
+mov ebx, snippet
+mov eax, size_snippet
+sub eax, 2
+
+DoMore: 
+cmp byte [ebx], ' '
+je after
+add byte [ebx], 32
+after:
+inc ebx
+dec eax
+jnz DoMore
+
+mov eax, 4
+mov ebx, 1
+mov ecx, snippet
+mov edx, size_snippet
+int 80h
+
+mov eax, 1
+mov ebx, 0
+int 80h
