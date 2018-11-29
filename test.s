21de833660b4a4f051b0df3acc4a434762b658b8
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-11-28 22:26:45 -0200

    multiply vector

--- test.s
+++ test.s
@@ -5,30 +5,30 @@ result resq 1
 
 .DATA
 
-op1 dq 1h
-op2 dq 2h
-msg_overflow db "Infelizmente teve overflow na soma de dois numeros de 64 bits", 0
+vector dd 0, 1, 2, 3, 4, 5, 6, 82, 9, 7, 8
+vector_size equ ($-vector)/4
 
 .CODE
 .STARTUP
 
-mov dword eax, [op1]
-mov dword ebx, [op1 + 4]
-mov dword ecx, [op2]
-mov dword edx, [op2 + 4]
+mov ecx, vector_size
+mov ebx, 0
+mov edx, 2
 
-add eax, ecx
-jnc after_overflow
-PutStr msg_overflow
-nwln
+multiply:
+mov dword eax, [vector + ebx * 4]
+imul dl
+mov [vector + ebx * 4], ax
+inc ebx
+loop multiply
 
-after_overflow:
-mov dword [result], eax
-mov dword [result + 4], ebx
+mov ecx, vector_size
+mov ebx, 0
 
-PutLInt [result+4]
-PutLInt [result]
-
+print_loop:
+mov dword eax, [vector + ebx * 4]
+PutInt ax
+inc ebx
+loop print_loop
 nwln
 .EXIT
-
